b27e91ff204582e7e7f7f01db8e457404bd104cf
Yusupov_dev <yusupovabdulaziz222@gmail.com>
2025-09-11 23:56:12 +0500

    db.json added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Home from "./components/Home/Home"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NotFound from './components/NotFound/NotFound'
+
 function App() {
   return (
     <Sidebar>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </Sidebar>
+
   )
 }
 
 export default App
+
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            <Typography color='white' variant='h2' >404 Page Not Found</Typography>
+        </div>
+    )
+}
+
+export default NotFound
